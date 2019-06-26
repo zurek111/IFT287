c976423ef90f1611f20268b14647d336f6d0a825
Maxime Pare <Damax55@hotmail.com>
2019-06-26 09:49:14 -0400

    TableClient faite pis t tellement gay de pas etre venu

--- TP3/src/AubergeInn/Table/TableClients.java
+++ TP3/src/AubergeInn/Table/TableClients.java
@@ -1,26 +1,24 @@
 package AubergeInn.Table;
 
-import java.sql.*;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import AubergeInn.Connexion;
 import AubergeInn.Tuple.TupleClient;
 
 public class TableClients 
 {
-    private PreparedStatement stmExist;
-    private PreparedStatement stmInsert;
-    private PreparedStatement stmDelete;
+    private TypedQuery<TupleClient> stmClient;
+
 
     private Connexion cx;
     
-	public TableClients(Connexion cx) throws SQLException 
+	public TableClients(Connexion cx)
 	{
 		this.cx = cx;
-		stmExist = cx.getConnection().prepareStatement(
-				"select idClient, prenom, nom, age from Client where idClient = ?");
-		stmInsert = cx.getConnection().prepareStatement(
-				"insert into Client (idClient, prenom, nom, age) values (?,?,?,?)");
-		stmDelete = cx.getConnection().prepareStatement(
-				"delete from Client where idClient = ?");
+		stmClient = cx.getConnection().createQuery(
+				"select c from Client where c.c_idClient = :idClient",TupleClient.class);
 	}
 	
     public Connexion getConnexion()
@@ -29,43 +27,32 @@ public class TableClients
     }
     
     /**
-	 * Fonction pour savoir si un client fait parti de la BD.
+	 * Fonction pour savoir si un client fait parti de la BD objet.
 	 * 
 	 * @param idClient  l'id du client.
 	 * 
 	 * @return Vrai si le client existe, faux sinon.
      */
-    public boolean existe(int idClient) throws SQLException
+    public boolean existe(int idClient)
     {
-    	stmExist.setInt(1, idClient);
-    	ResultSet rst = stmExist.executeQuery(); 	
-    	boolean clientExist = rst.next();
-    	rst.close();
-   
-    	return clientExist;
+    	stmClient.setParameter("idClient", idClient);
+    	return !stmClient.getResultList().isEmpty();
     }
     
     /**
-	 * Fonction pour obtenir un client de la BD.
+	 * Fonction pour obtenir un client de la BD objet.
 	 * 
 	 * @param idClient  l'id du client.
 	 * 
 	 * @return Le tuple du client contenant ses données.
      */
-    public TupleClient getClient(int idClient) throws SQLException
+    public TupleClient getClient(int idClient)
     {
-    	stmExist.setInt(1, idClient);
-        ResultSet rst = stmExist.executeQuery();
-        if (rst.next())
+    	stmClient.setParameter("idClient", idClient);
+    	List<TupleClient> clients = stmClient.getResultList();
+        if(!clients.isEmpty())
         {
-            TupleClient tupleClient = new TupleClient();
-            tupleClient.setIdClient(idClient);
-            tupleClient.setPrenom(rst.getString(2));
-            tupleClient.setNom(rst.getString(3));
-            tupleClient.setAge(rst.getInt(4));
-            rst.close();
-            
-            return tupleClient;
+            return clients.get(0);
         }
         else
         {
@@ -76,35 +63,30 @@ public class TableClients
     /**
 	 * Fonction pour ajouter un client dans la BD.
 	 * 
-	 * @param idClient  l'id du client.
-	 * @param prenom  le nom du client.
-	 * @param nom  le prenom du client.
-	 * @param age  l'age du client.
+	 * @param TupleClient L'objet client à ajouter dans la BD.
 	 * 
-	 * @return Le nombre de lignes ajoutées dans la BD.
+	 * @return L'objet Client ajouté dans la BD.
      */
-    public int ajouter(int idClient, String prenom, String nom, int age) throws SQLException
+    public TupleClient ajouter(TupleClient client)
     {
-    	stmInsert.setInt(1, idClient);
-    	stmInsert.setString(2, prenom);
-    	stmInsert.setString(3, nom);
-    	stmInsert.setInt(4, age);
-    	return stmInsert.executeUpdate();
+    	cx.getConnection().persist(client);
+    	return client;
     }
     
     /**
 	 * Fonction pour supprimer un client de la BD.
 	 * 
-	 * @param idClient  l'id du client.
-	 * @param prenom  le nom du client.
-	 * @param nom  le prenom du client.
-	 * @param age  l'age du client.
+	 * @param TupleClient L'objet client à ajouter dans la BD.
 	 * 
-	 * @return Le nombre de lignes supprimées dans la BD.
+	 * @return retourne vrai si supprimer dans la BD faux sinon.
      */
-    public int supprimer(int idClient) throws SQLException
+    public boolean supprimer(TupleClient client)
     {
-    	stmDelete.setInt(1, idClient);
-    	return stmDelete.executeUpdate();
+    	if(client != null)
+        {
+            cx.getConnection().remove(client);
+            return true;
+        }
+        return false;
     }
 }
