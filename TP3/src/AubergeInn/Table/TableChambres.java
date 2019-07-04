7ea93073dd182d8a1f09cdebe4a65579ce75cf92
unknown <Damax55@hotmail.com>
2019-07-03 21:10:07 -0400

    tableChambre

--- TP3/src/AubergeInn/Table/TableChambres.java
+++ TP3/src/AubergeInn/Table/TableChambres.java
@@ -1,43 +1,30 @@
 package AubergeInn.Table;
 
 import java.sql.*;
-import java.util.LinkedList;
 import java.util.List;
+
+import javax.persistence.TypedQuery;
 
 import AubergeInn.Connexion;
 import AubergeInn.Tuple.TupleChambre;
 
 public class TableChambres 
 {
-    private PreparedStatement stmSelect;
-    private PreparedStatement stmExist;
-    private PreparedStatement stmCommoditeIncluse;
-    private PreparedStatement stmRemoveCommodite;
-    private PreparedStatement stmInsert;
-    private PreparedStatement stmDelete;
-    private PreparedStatement stmInclude;
+    private TypedQuery<TupleChambre> stmSelect;
+    private TypedQuery<TupleChambre> stmExist;
+
 
     private Connexion cx;
     
     // fonctions de connexion
-	public TableChambres(Connexion cx) throws SQLException 
+	public TableChambres(Connexion cx)
 	{
 		this.cx = cx;
 		
-		stmSelect = cx.getConnection().prepareStatement(
-				"select * from Chambre");
-		stmExist = cx.getConnection().prepareStatement(
-				"select * from Chambre where idChambre = ?");
-		stmInsert = cx.getConnection().prepareStatement(
-				"insert into Chambre (idChambre, nom, typeLit, prix) values (?,?,?,?)");
-		stmDelete = cx.getConnection().prepareStatement(
-				"delete from Chambre where idChambre = ?");	
-		stmInclude = cx.getConnection().prepareStatement(
-				"insert into CommoditeOfferte (idChambre, idCommodite) values (?,?)");
-		stmCommoditeIncluse = cx.getConnection().prepareStatement(
-				"select * from CommoditeOfferte where idChambre = ? and idCommodite = ?");
-		stmRemoveCommodite = cx.getConnection().prepareStatement(
-				"delete from CommoditeOfferte where idChambre = ? and idCommodite = ?");
+		stmSelect = cx.getConnection().createQuery(
+				"select c from Chambre c",TupleChambre.class);
+		stmExist = cx.getConnection().createQuery(
+				"select c from Chambre c where c.idChambre = :idChambre",TupleChambre.class);
 	}
 	
     public Connexion getConnexion()
@@ -52,14 +39,11 @@ public class TableChambres
 	 * 
 	 * @return Vrai si la chambre existe, faux sinon.
      */
-    public boolean existe(int idChambre) throws SQLException
+    public boolean existe(int idChambre)
     {
-    	stmExist.setInt(1, idChambre);
-    	ResultSet rst = stmExist.executeQuery();
-    	boolean chambreExist = rst.next();
-    	rst.close();
+    	stmExist.setParameter("idChambre", idChambre);
     	
-    	return chambreExist;
+    	return !stmExist.getResultList().isEmpty();
     }
     
     /**
@@ -70,7 +54,7 @@ public class TableChambres
 	 * 
 	 * @return Vrai si la commodité est incluse dans la chambre, faux sinon.
      */
-    public boolean commoditeIncluse(int idChambre, int idCommodite) throws SQLException
+    /*public boolean commoditeIncluse(int idChambre, int idCommodite) throws SQLException
     {
     	stmCommoditeIncluse.setInt(1, idChambre);
     	stmCommoditeIncluse.setInt(2, idCommodite);
@@ -79,26 +63,17 @@ public class TableChambres
     	rst.close();
     	
     	return commoditeChambreExist;
-    }
+    }*/
     /**
 	 * Fonction pour obtenir toutes les chambres de la BD.
 	 * 
 	 * @return La liste des chambres.
      */
-    public List<TupleChambre> getAllChambre() throws SQLException
+    public List<TupleChambre> getAllChambre()
     {
-    	ResultSet rset = stmSelect.executeQuery();
-    	List<TupleChambre> listeChambres = new LinkedList<TupleChambre>();
-        while (rset.next())
-        {
-        	TupleChambre chambre = new TupleChambre(rset.getInt("idChambre"),
-                                                    rset.getString("nom"),
-                                                    rset.getString("typeLit"),
-                                                    rset.getInt("prix"));
-            
-        	listeChambres.add(chambre);
-        }
-        rset.close();
+    	
+    	List<TupleChambre> listeChambres = stmSelect.getResultList();
+        
         return listeChambres;
     }
     
@@ -109,20 +84,13 @@ public class TableChambres
 	 * 
 	 * @return Le tuple de la chambre qui contient les données de celle-ci.
      */
-    public TupleChambre getChambre(int idChambre) throws SQLException
+    public TupleChambre getChambre(int idChambre)
     {
-    	stmExist.setInt(1, idChambre);
-        ResultSet rst = stmExist.executeQuery();
-        if (rst.next())
+    	stmExist.setParameter("idChambre", idChambre);
+        List<TupleChambre> chambres = stmExist.getResultList();
+        if (!chambres.isEmpty())
         {
-            TupleChambre tupleChambre = new TupleChambre();
-            tupleChambre.setIdChambre(idChambre);
-            tupleChambre.setNom(rst.getString(2));
-            tupleChambre.setTypeLit(rst.getString(3));
-            tupleChambre.setPrix(rst.getInt(4));
-            rst.close();
-            
-            return tupleChambre;
+            return chambres.get(0);
         }
         else
         {
@@ -138,15 +106,12 @@ public class TableChambres
 	 * @param typeLit  le type de lit de la chambre.
 	 * @param prix  le prix de la chambre.
 	 * 
-	 * @return Le nombre de lignes ajoutées dans la BD.
+	 * @return La chambre ajoutée dans la BD.
      */
-    public int ajouter(int idChambre, String nom, String typeLit, int prix) throws SQLException
+    public TupleChambre ajouter(TupleChambre chambre)
     {
-    	stmInsert.setInt(1, idChambre);
-    	stmInsert.setString(2, nom);
-    	stmInsert.setString(3, typeLit);
-    	stmInsert.setInt(4, prix);
-    	return stmInsert.executeUpdate();
+    	cx.getConnection().persist(chambre);
+    	return chambre;
     }
     
     /**
@@ -154,12 +119,16 @@ public class TableChambres
 	 * 
 	 * @param idChambre  l'id de la chambre.
 	 * 
-	 * @return Le nombre de lignes supprimées dans la BD.
+	 * @return vrai si supprimées dans la BD faux sinon.
      */
-    public int supprimer(int idChambre) throws SQLException
+    public boolean supprimer(TupleChambre chambre) throws SQLException
     {
-    	stmDelete.setInt(1, idChambre);
-    	return stmDelete.executeUpdate();
+    	if (chambre != null)
+    	{
+    		cx.getConnection().remove(chambre);
+    		return true;
+    	}
+    	return false;
     }
     
     /**
@@ -170,12 +139,12 @@ public class TableChambres
 	 * 
 	 * @return Le nombre de lignes ajoutées dans la BD.
      */
-    public int inclureCommodite(int idChambre,int idCommodite) throws SQLException
+    /*public int inclureCommodite(int idChambre,int idCommodite) throws SQLException
     {
     	stmInclude.setInt(1, idChambre);
     	stmInclude.setInt(2, idCommodite);
     	return stmInclude.executeUpdate();
-    }
+    }*/
     
     /**
 	 * Fonction pour retirer une commodité d'une chambre.
@@ -185,10 +154,10 @@ public class TableChambres
 	 * 
 	 * @return Le nombre de lignes supprimées dans la BD.
      */
-    public int enleverCommodite(int idChambre,int idCommodite) throws SQLException
+    /*public int enleverCommodite(int idChambre,int idCommodite) throws SQLException
     {
     	stmRemoveCommodite.setInt(1, idChambre);
     	stmRemoveCommodite.setInt(2, idCommodite);
     	return stmRemoveCommodite.executeUpdate();
-    }
+    }*/
 }
